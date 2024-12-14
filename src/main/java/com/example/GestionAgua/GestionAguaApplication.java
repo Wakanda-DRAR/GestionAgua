3cc237a8545328cf1ae6d113e8f130e2b586e8f7
dxn1l <danielmorenorey2012@gmail.com>
2024-12-14 19:10:14 +0100

    Initial commit

---
+++ src/main/java/com/example/GestionAgua/GestionAguaApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionAgua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAguaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GestionAgua/GestionAguaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionAgua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAguaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
